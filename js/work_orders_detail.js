cc21afa7f91aee69dc91991d419d42c6dca4d575
xiongjunlang <xiongjunlang@agree.com.cn>
2017-12-20 14:16:41 +0800

    增加支付宝、微信支付的二维码;修复点击添加活动时页面是空白的bug;修改介绍页面的人物介绍部分;如果库存为0则使按钮失效

--- js/work_orders_detail.js
+++ js/work_orders_detail.js
@@ -1,36 +1,36 @@
-$(function() {
-	$("#gotoOrder").on("click",function(event){
-		let workId=$(event.target).data("workid");
-		window.location.href="/order/toAddOrder?workId="+workId;
-	});
-            var widthget = 0;
-            if (window.innerWidth) {
-                widthget = window.innerWidth;
-            }
-            else ((document.body) && (document.body.clientWidth))
-            {
-                widthget = document.body.clientWidth;
-            }
-            // window.alert(widthget);
-            if (document.getElementById("mybar")){
-                if(widthget<768)
-                {
-                    document.getElementById("shownav").style ="background-color:rgba(0, 73, 198, 0.55)";
-                    document.getElementById("jieshaoh1").style = "padding-top: 2em; font-size:2em;";
-                    document.getElementById("jieshaoh2").style = "padding-bottom: 2em;font-size: large;"
-                }
-                if(widthget>=500 && widthget<1100)
-                {
-
-                    document.getElementById("jieshaoh1").style = "padding-top: 3em;font-size:xx-large";
-                    document.getElementById("jieshaoh2").style = "padding-bottom: 3em; font-size:x-large;"
-                }
-                if(widthget>=1100)
-                {
-                    document.getElementById("jieshaoh1").style = "padding-top: 4em;font-size:xx-large;";
-                    document.getElementById("jieshaoh2").style = "padding-bottom: 4em;font-size: x-large;"
-
-                }
-            }
+$(function () {
+    if($("#stock").text()=="库存:0"){
+        $("#gotoOrder").attr("disabled",true);
+    }
+    $("#gotoOrder").on("click", function (event) {
+        let workId = $(event.target).data("workid");
+        window.location.href = "/order/toAddOrder?workId=" + workId;
+    });
+    var widthget = 0;
+    if (window.innerWidth) {
+        widthget = window.innerWidth;
+    }
+    else ((document.body) && (document.body.clientWidth))
+    {
+        widthget = document.body.clientWidth;
+    }
+    // window.alert(widthget);
+    if (document.getElementById("mybar")) {
+        if (widthget < 768) {
+            document.getElementById("shownav").style = "background-color:rgba(0, 73, 198, 0.55)";
+            document.getElementById("jieshaoh1").style = "padding-top: 2em; font-size:2em;";
+            document.getElementById("jieshaoh2").style = "padding-bottom: 2em;font-size: large;"
         }
+        if (widthget >= 500 && widthget < 1100) {
+
+            document.getElementById("jieshaoh1").style = "padding-top: 3em;font-size:xx-large";
+            document.getElementById("jieshaoh2").style = "padding-bottom: 3em; font-size:x-large;"
+        }
+        if (widthget >= 1100) {
+            document.getElementById("jieshaoh1").style = "padding-top: 4em;font-size:xx-large;";
+            document.getElementById("jieshaoh2").style = "padding-bottom: 4em;font-size: x-large;"
+
+        }
+    }
+}
 );
