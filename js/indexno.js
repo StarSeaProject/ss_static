ab278e602579747d61cbe94b50d03189648df972
zcz1997 <zcz1997@hotmail.com>
2017-12-27 16:10:18 +0800

    主页样式表咻改

--- js/indexno.js
+++ js/indexno.js
@@ -9,34 +9,11 @@
         }
         if(widthget>500&&widthget<1100)
         {
-            document.getElementById("homepage").style.height = window.innerHeight + "px";
+            document.getElementById("homepage").style.height = 800+"px";
         }
         if(widthget>1100)
         {
-            document.getElementById("homepage").style.height = widthget * 0.5 + "px";
-        }
-
-        if(widthget<500)
-        {
-            document.getElementById("homeword").style.paddingTop = 130 + "px";
-            document.getElementById("jieshao").className = "container jieshao";
-            document.getElementById("jieshaoh1").style = "padding-top: 2em; font-size:2em;"
-            document.getElementById("jieshaoh2").style = "padding-bottom: 2em;font-size: large;"
-        }
-        if(widthget>=500 && widthget<1500)
-        {
-            document.getElementById("homeword").style.paddingTop = 400 + "px";
-            document.getElementById("jieshao").className = "container jieshao container jieshao col-xs-6 col-xs-offset-2";
-            document.getElementById("jieshaoh1").style = "padding-top: 3em;font-size:xx-large"
-            document.getElementById("jieshaoh2").style = "padding-bottom: 3em; font-size:x-large;"
-        }
-        if(widthget>=1500)
-        {
-            document.getElementById("homeword").style.paddingTop = widthget*0.4 + "px";
-            document.getElementById("jieshao").className = "container jieshao container jieshao col-xs-6 col-xs-offset-3";
-            document.getElementById("jieshaoh1").style = "padding-top: 4em;font-size:xx-large;"
-            document.getElementById("jieshaoh2").style = "padding-bottom: 4em;font-size: x-large;"
-
+            document.getElementById("homepage").style.height = 800+ "px";
         }
 
 })();
