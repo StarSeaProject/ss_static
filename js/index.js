828214519b9f8cec75ad977719f8617083b966b2
zcz1997 <zcz1997@hotmail.com>
2018-01-08 11:15:37 +0800

    主页下拉导航条半透明效果

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 if(window.innerWidth<500)
-		document.getElementById("shownav").style="background:rgba(211,211,211,0.5)";
+    document.getElementById("shownav").style="background:rgba(211,211,211,0.5)";
 (function(){
     pos = 0;
     ticking = false;
@@ -7,12 +7,17 @@ if(window.innerWidth<500)
     window.addEventListener("scroll", function(){
         pos = window.scrollY;
         if(pos > 70&&!ticking){
+
             header.classList.add("scrolledDown");
+
             ticking = true;
+            $("#mybar").attr("style","background:rgba(99,99,99,0.7)");
         }
         if(pos < 70 && ticking){
+
             header.classList.remove("scrolledDown");
             ticking = false;
+            $("#mybar").attr("style","background:transient");
         }
     });
 })();
