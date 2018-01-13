a4f062c39641ac38eabd556601bec0a913457eba
zcz1997 <zcz1997@hotmail.com>
2018-01-14 02:53:26 +0800

    作品页图片大小控制

--- js/read.js
+++ js/read.js
@@ -1,45 +1,45 @@
 $(function () {
-	$("#works").on("click", function (event) {
-		let workId = $(event.target).data("workid");
-		if (workId == undefined)
-			return;
-		$("#workId").val(workId);
-		$("#workDetail").submit();
-	});
-	$("#pages").on("click", function (event) {
-		let page = $(event.target).text();
-		$(event.target).attr("href", "/work/?page=" + page);
-		$(event.target).click();
-	});
+    $("#works").on("click", function (event) {
+        let workId = $(event.target).data("workid");
+        if (workId == undefined)
+            return;
+        $("#workId").val(workId);
+        $("#workDetail").submit();
+    });
+    $("#pages").on("click", function (event) {
+        let page = $(event.target).text();
+        $(event.target).attr("href", "/work/?page=" + page);
+        $(event.target).click();
+    });
 });
 let page = 2;
 let total;
 $(window).scroll(function () {
-	let scrollTop = $(this).scrollTop();
-	let scrollHeight = $(document).height();
-	let windowHeight = $(this).height();
-	if (scrollTop + windowHeight == scrollHeight) {
-		if (page > total)
-			return;
-		$.ajax({
-			type: "post",
-			url: "/work/ajax",
-			contentType: "application/json;charset=utf-8",
-			dataType: "json",
-			data: '{"workName":"","page":"' + page + '"}',
-			success: function (data) {
-				if (data.result === undefined)
-					return;
-				let str = "";
-				page++;
-				total = data.totalPage;
-				for (let work of data.result) {
-					str += `<div>
+    let scrollTop = $(this).scrollTop();
+    let scrollHeight = $(document).height();
+    let windowHeight = $(this).height();
+    if (scrollTop + windowHeight == scrollHeight) {
+        if (page > total)
+            return;
+        $.ajax({
+            type: "post",
+            url: "/work/ajax",
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            data: '{"workName":"","page":"' + page + '"}',
+            success: function (data) {
+                if (data.result === undefined)
+                    return;
+                let str = "";
+                page++;
+                total = data.totalPage;
+                for (let work of data.result) {
+                    str += `<div>
 						<div class="row workbox">
-							<div class="col-xs-3 workimage">
+							<div class="col-xs-5 workimage">
 								<img src="${work.workCover}" class="workimg img-reponsive"></img>
 							</div>
-							<div class="col-xs-9 workdetail">
+							<div class="col-xs-7 workdetail">
 								<p  class="h3 worktitle">${work.workName}</p>
 								<p class="p workdetail">${work.workSummary}</p>
 								<a data-workid=${work.workId} class="h4 detailhref" href="javascript:void(0)">&nbsp;&nbsp;查看作品&nbsp;&nbsp;</a>
@@ -47,9 +47,9 @@ $(window).scroll(function () {
 						</div>
 						<hr/>
 					</div>`;
-				}
-				$("#works").append(str);
-			}
-		});
-	}
+                }
+                $("#works").append(str);
+            }
+        });
+    }
 });
