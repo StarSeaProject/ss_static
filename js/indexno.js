3ed85cbeddd65cd09aa1d7fca48f03d91eecbd7a
zcz1997 <zcz1997@hotmail.com>
2018-01-24 22:07:18 +0800

    主页背景高度bug修复

--- js/indexno.js
+++ js/indexno.js
@@ -1,22 +1,20 @@
 (function () {
-    var widthget = 0;
-    if (window.innerWidth) {
-        widthget = window.innerWidth;
-    }
+    var widthget = window.innerWidth;
+    
     switch (widthget) {
         case 1280:
-            document.getElementById("homepage").style.height = 600 + "px";
+            document.getElementById("homepage").style.height = 600 + "px";break;
         case 1366:
-            document.getElementById("homepage").style.height = 600 + "px";
+            document.getElementById("homepage").style.height = 600 + "px";break;
         case 1920:
-            document.getElementById("homepage").style.height = 800 + "px";
+            document.getElementById("homepage").style.height = 800 + "px";break;
         case 1024:
-            document.getElementById("homepage").style.height = 600 + "px";
+            document.getElementById("homepage").style.height = 600 + "px";break;
         case 1600:
-            document.getElementById("homepage").style.height = 700 + "px";
-		case 2160:
-			document.getElementById("homepage").style.height = 900 + "px";
-		case 3840:
-			document.getElementById("homepage").style.height = 1600 + "px";
+            document.getElementById("homepage").style.height = 700 + "px";break;
+        case 2160:
+            document.getElementById("homepage").style.height = 900 + "px";break;
+        default:
+            document.getElementById("homepage").style.height = 600 + "px";break;
     }
 })();
