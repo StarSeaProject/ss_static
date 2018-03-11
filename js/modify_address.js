06dc4a2a5d4b04b02f4297d1920843f1f78e5a22
liuyang <liuyang@Lenovo-PC>
2018-03-11 22:50:55 +0800

    增加修改收货地址的js文件

---
+++ js/modify_address.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    $("#modifyAddressBtn").on("click",function(e){  
+        areanum = $("#area").val(); 
+        if(areanum==""||areanum==undefined){
+        	alert("收货地址不合法！");
+        	return false;
+        }
+        $("#areasub").val(areanum);
+        adddetail = $("#ordadddel").val();
+        if(adddetail.length==0){
+        	alert("收货详细地址不能为空！");
+        	return false;
+        }
+        if(adddetail.length>=50){
+        	alert("收货地址过长！");
+        	return false;
+        }
+        $("#modifyAddressForm").submit(); 
+    });
+});
