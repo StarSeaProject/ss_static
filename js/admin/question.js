16b522825ae440f7d581ec5b3e36fbed579b121b
theeditor <syua9708@flhs.us>
2018-03-12 16:57:48 -0400

    修复react爆炸的问题

--- js/admin/question.js
+++ js/admin/question.js
@@ -1,144 +1,56 @@
-// class QuestionTable extends React.Component{
-//     render(){
-//         return(
-//             <table id="questions" class="table">
-//                 <thead>
-//                 <tr>
-//                     <th>问题内容</th>
-//                     <th>问题提出时间</th>
-//                     <th>问题状态</th>
-//                     <th>操作</th>
-//                 </tr>
-//                 </thead>
-//                 <tbody></tbody>
-//             </table>
-//         )
-//     }
-
-
-// }
-// class Page extends React.Component{
-//     constructor(props){
-//         super(props);
-//         this.handleClick = this.handleClick.bind(this);
-//         let page = 1;
-//         let ajaxOpt = {
-//             type: "post",
-//             url: "question/ajax",
-//             contentType: "application/json;charset=utf-8",
-//             dataType: "json",
-//             data: '{"questionStatus":"' + $("#questionStatus").val() + '","page":"' + page + '"}',
-//             async: false,
-//             success: handleData,
-//         };
-//         $.ajax(ajaxOpt);
-//     }
-//     handleClick(e){
-//         if (isNaN($(event.target).text())) {
-//             return;
-//         }
-//         page = $(event.target).text();
-//         // ajaxOpt.data = '{"questionStatus":"' + $("#questionStatus").val() + '","page":"' + page + '"}';
-//         ajaxOpt.data = JSON.stringify({
-//             questionStatus:$("#questionStatus").val(),
-//             page
-//         })
-//         $.ajax(ajaxOpt);
-//     }
-//     render() {
-//         return (
-//             <div id="pages" onClick={this.handleClick}>
-//                 {
-//                     this.props.nowPage - 1 > 0 && <a href="javascript:void(0)">{this.props.nowPage - 1}</a>
-//                 }
-//                 <span>{this.props.nowPage}</span>
-//                 {
-//                     this.props.nowPage + 1 <= this.props.totalPage && <a href="javascript:void(0)">{this.props.nowPage + 1}</a>
-//                 }
-//             </div>
-//         );
-//     }
-// }
-// class Question extends React.Component{
-//     constructor(props) {
-//         super(props);
-//         this.handleQueryClick = this.handleQueryClick.bind(this);
-//     }
-//     handleQueryClick(){
-//         $.ajax({
-//             type: "post",
-//             url: "question/ajax",
-//             contentType: "application/json;charset=utf-8",
-//             dataType: "json",
-//             data: JSON.stringify({
-//                 questionStatus: this.props.questionStatus
-//             }),
-//             async: false,
-//             success: (data) => {
-//                 ReactDOM.render(
-//                     <div>
-//                         <div class="row qaBox">
-//                             <div class="col-sm-12 col-md-10 qBox">
-//                                 <p class="qText">
-//                                     Q:${data.question}
-//                                 </p>
-//                             </div>
-//                             <div class="col-sm-12 col-md-10 aBox">
-//                                 <p class="aText">
-//                                     A:${data.answer}
-//                                 </p>
-//                             </div>
-//                         </div>
-//                     </div>,
-//                     document.getElementById("findresult")
-//                 );
-//             },
-//             error: (data) => {
-//                 console.log(data);
-//             }
-//         });
-//     }
-// }
-
 class Question extends React.Component {
-    constructor(props){
-        super(props)
+    constructor(props) {
+        super(props);
     }
-    render(){
-        return(
+    componentWillMount() {
+        let page = 1;
+        let ajaxOpt = {
+            type: "post",
+            url: "question/ajax",
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            data: '{"questionStatus":"' + 0 + '","page":"' + page + '"}',
+            async: false,
+            success: (data) => {
+                this.props.questionArr = data
+            },
+        };
+        $.ajax(ajaxOpt);
+    }
+    render() {
+        return (
             <div className="container">
                 <QuestionOption />
-                <QuestionTable />
-                <Page />
-        </div>
+                <QuestionTable data={this.props.questionArr} />
+            </div>
         )
     }
 }
 
-class QuestionOption extends React.Component{
+class QuestionOption extends React.Component {
     constructor(props) {
         super(props)
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className="row container form">
-                    <select id="questionStatus">
-                        <option value="">请选择问题状态</option>
-                        <option value="1">未回答</option>
-                        <option value="2">已回答</option>
-                    </select>
+                <select id="questionStatus">
+                    <option value="">请选择问题状态</option>
+                    <option value="1">未回答</option>
+                    <option value="2">已回答</option>
+                </select>
                 <button id="questionquery" className="btn btn-info btn-lg text-right">查询</button>
-             </div>
+            </div>
         )
     }
 }
 
-class QuestionTable extends React.Component{
-    constructor(props){
+class QuestionTable extends React.Component {
+    constructor(props) {
         super(props)
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className="row container">
                 <table id="questions" className="table">
                     <thead>
@@ -150,30 +62,72 @@ class QuestionTable extends React.Component{
                         </tr>
                     </thead>
                     <tbody>
-                        <QuestionTableThread />
+                        {
+                            this.props.data.result.map((data, index) => {
+                                return <QuestionTableThread data={data} />
+                            })
+                        }
                     </tbody>
                 </table>
-                <div id="pages"></div>
+                <Page data={this.props.data} />
             </div>
         )
     }
 }
 
-class QuestionTableThread extends React.Component{
-    constructor(props){
-        super(props)
+class QuestionTableThread extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this)
     }
-    render(){
-        return(
+
+    handleClick() {
+        var answer = prompt("请输入回复内容", "");
+        if (answer === undefined || answer === "") {
+            alert("请输入回复内容！");
+            return false;
+        }
+        if (answer.length > 150) {
+            alert("回复内容不能超过150！");
+            return false;
+        }
+        let questionId = this.props.data.questionId
+        if (questionId === undefined || questionId === "") {
+            alert("问题id不能为空");
+            return false;
+        }
+        $.ajax({
+            type: "post",
+            // data: '{"questionId":"' + questionId + '","answer":"' + answer + '"}',
+            data:JSON.stringify({
+                questionId,answer
+            }),
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            url: "question/answer",
+            success: function (data) {
+                if (data.result == true) {
+                    alert("回复成功!");
+                    location.reload();
+                }
+                else {
+                    alert("回复失败!");
+                }
+            }
+        });
+    }
+
+    render() {
+        return (
             <tr>
-                <td>{this.props.question}</td>
-                <td>{this.props.createTime}</td>
-                <td className={this.props.questionId}>{this.props.questionStatus}</td>
+                <td>{this.props.data.question}</td>
+                <td>{this.props.data.createTime}</td>
+                <td className={this.props.data.questionId}>{this.props.data.questionStatus}</td>
                 {
-                    this.props.questionStatus == "未回答" &&
-                        <td>
-                            <button data-questionid={this.props.questionId} class="answer btn btn-primary btn-lg text-right" id={this.props.questionId}>回答</button>
-                        </td>
+                    this.props.data.questionStatus == "未回答" &&
+                    <td>
+                        <button onClick={this.handleClick} className="answer btn btn-primary btn-lg text-right" id={this.props.data.questionId}>回答</button>
+                    </td>
                 }
             </tr>
         )
@@ -181,19 +135,19 @@ class QuestionTableThread extends React.Component{
 }
 
 
-class Page extends React.Component{
-    constructor(props){
+class Page extends React.Component {
+    constructor(props) {
         super(props)
     }
-    render(){
-        return(
+    render() {
+        return (
             <div id="pages" onClick={this.handleClick}>
                 {
-                    this.props.nowPage - 1 > 0 && <a href="javascript:void(0)">{this.props.nowPage - 1}</a>
+                    this.props.data.nowPage - 1 > 0 && <a href="javascript:void(0)">{this.props.nowPage - 1}</a>
                 }
                 <span>{this.props.nowPage}</span>
                 {
-                    this.props.nowPage + 1 <= this.props.totalPage && <a href="javascript:void(0)">{this.props.nowPage + 1}</a>
+                    this.props.data.nowPage + 1 <= this.props.totalPage && <a href="javascript:void(0)">{this.props.nowPage + 1}</a>
                 }
             </div>
         )
