3af5147c56e14aef853098f26141db4e279270a0
xiongjunlang <xiongjunlang@agree.com.cn>
2018-03-26 14:33:48 +0800

    添加作品列表页和详细页的作品类型逻辑js

--- js/work_detail.js
+++ js/work_detail.js
@@ -1,5 +1,49 @@
+$(function () {
+    let str = `<img style="width: 70%" src="${workImages.shift().workImagePath}" /><br/>`;
+    $("#more_image").before(str);
+    $("#more_image").on("click", () => {
+        let str = `<img style="width: 70%" src="${workImages.shift().workImagePath}" /><br/>`;
+        $("#more_image").before(str);
+    });
+
+    $("#gotoOrder").on("click", function (event) {
+        $.ajax({
+            url: "/car/add",
+            type: "post",
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            data: '{"workId":"' + $(event.target).data("workid") + '","workTypeId":"' + $(".active").data("worktypeid") + '"}',
+            async: false,
+            success: function (data) {
+                modalalert(data.info);
+            }
+        });
+    });
+
+    let sum = 0;
+    for (let workType of workTypes) {
+        sum += workType.stock;
+    }
+    $("#stock").data("sum", sum);
+    $("#stock").text("  共" + sum + "个");
+    $("#workType").on("click", function (e) {
+        let stock = $(e.target).data("stock");
+        if (stock == 0) {
+            $("#stock").text("没有库存啦");
+            $("#gotoOrder").attr("disabled", "disabled");
+        } else if (stock != 0 && stock != undefined) {
+            $("#stock").text("  还有" + stock + "个");
+        }
+        let isDisabled = $("#gotoOrder").attr("disabled") == "disabled";
+        if (isDisabled) {
+            if (stock > 0) {
+                $("#gotoOrder").removeAttr("disabled");
+            }
+        }
+    });
+});
 $(window).scroll(function () {
-    y = $(window).height();
-    cury = $(window).scrollTop()+y-160;
-    $('#cardiv').css("left","90%").css("top",cury+"px");
+    const y = $(window).height();
+    const cury = $(window).scrollTop() + y - 160;
+    $('#cardiv').css("left", "90%").css("top", cury + "px");
 });
