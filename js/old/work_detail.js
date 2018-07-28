35c5eb138446fa30e280af6f2512a2634917ce82
xiongjunlang <xiongjunlang@agree.com.cn>
2018-07-28 17:19:55 +0800

    优化兼容性js代码

--- js/old/work_detail.js
+++ js/old/work_detail.js
@@ -1,56 +1,64 @@
 document.onreadystatechange = function () {
-    if (document.readyState == 'loader' || document.readyState == 'complete' || document.readyState == 'interactive') {
-        initWorkDetail();
+    if (document.readyState == 'complete') {
+        initWorkDetail(workImages, workTypes);
     }
 }
-function initWorkDetail() {
+function initWorkDetail(workImages, workTypes) {
     var moreImage = document.createElement("img");
     moreImage.src = workImages.shift().workImagePath;
-    var workImages = document.getElementById("workImages");
-    workImages.insertBefore(moreImage, workImages.lastChild);
+    moreImage.className = "more_picture";
+    var workImagesDiv = document.getElementById("workImages");
+    workImagesDiv.insertBefore(moreImage, workImagesDiv.lastChild);
     document.getElementById("more_image").onclick = function () {
         if (workImages.length == 0)
             return;
         var _moreImage = document.createElement("img");
         _moreImage.src = workImages.shift().workImagePath;
-        workImages.insertBefore(_moreImage, workImages.lastChild);
-    }
+        _moreImage.className = "more_picture";
+        workImagesDiv.insertBefore(_moreImage, workImagesDiv.lastChild);
+    };
 
-    document.getElementById("gotoOrder").onclick = function (event) {
+    var gotoOrderDiv = document.getElementById("gotoOrder");
+    gotoOrderDiv.onclick = function (e) {
+        var event = e || window.event;
+        var target = event.target || event.srcElement;
         var xhr = createXmlHttp();
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
-                var _data = xhr.responseText;
+                var _data = JSON.parse(xhr.responseText);
                 modalalert(_data.info);
             }
-        }
+        };
         xhr.open("post", "/car/add", true);
         xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8");
         var data = {
-            "workId": event.target.getAttribute("data-workid"),
-            "workTypeId": document.getElementsByClassName("active").getAttribute("data-worktypeid")
+            "workId": target.getAttribute("data-workid"),
+            "workTypeId": document.getElementsByClassName("active").item(0).getAttribute("data-worktypeid")
         };
         xhr.send(JSON.stringify(data));
-    }
+    };
 
     var sum = 0;
     for (var i = 0; i < workTypes.length; i++) {
         sum += workTypes[i].stock;
     }
-    document.getElementById("stock").setAttribute("data-sum", sum);
-    document.getElementById("stock").innerHTML = '  共' + sum + '个';
-    document.getElementById("workType").onclick = function (event) {
-        var stock = event.target.getAttribute("data-stock");
+    var stockDiv = document.getElementById("stock");
+    stockDiv.setAttribute("data-sum", sum);
+    stockDiv.innerHTML = '  共' + sum + '个';
+    document.getElementById("workType").onclick = function (e) {
+        var event = e || window.event;
+        var target = event.target || event.srcElement;
+        var stock = target.getAttribute("data-stock");
         if (stock == 0) {
-            document.getElementById("stock").innerHTML = '没有库存啦';
-            document.getElementById("gotoOrder").setAttribute("disabled", "disabled");
+            stockDiv.innerHTML = '没有库存啦';
+            gotoOrderDiv.setAttribute("disabled", "disabled");
         } else if (stock != 0 && stock != undefined) {
-            document.getElementById("stock").innerHTML = '  还有' + stock + "个";
+            stockDiv.innerHTML = '  还有' + stock + "个";
         }
-        var isDisabled = document.getElementById("gotoOrder").getAttribute("disabled") == "disabled";
+        var isDisabled = gotoOrderDiv.getAttribute("disabled") == "disabled";
         if (isDisabled) {
             if (stock > 0) {
-                document.getElementById("gotoOrder").removeAttribute("disabled");
+                gotoOrderDiv.removeAttribute("disabled");
             }
         }
     }
@@ -72,4 +80,4 @@ function createXmlHttp() {
         }
     }
     return xmlHttp;
-} 
+}
